9e370dbdaec8303410ad8e29c27d9ed09873daf9
PaoloGalliquio <paolo.galliquio@bit2bitamericas.com>
2022-09-03 19:02:10 -0500

    first commit

---
+++ src/main/java/com/redexbackend/redexbackend/RedexBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.redexbackend.redexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedexBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedexBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redexbackend/redexbackend/RedexBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redexbackend.redexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedexBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
